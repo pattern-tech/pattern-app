2008ca686214483c8d6eb51c356e08ceed1be741
Mohammad Kermani <mkermani144@gmail.com>
2025-02-26 16:14:51 +0000

    feat: switch to using Pattern backend for SIWE verification
    With this commit, the SSR acts simply as a proxy for auth, with the auth
    provider being the Pattern backend.

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -1,26 +1,64 @@
-import { NextAuthConfig } from "next-auth";
+import type { SIWESession } from '@reown/appkit-siwe';
+import type { NextAuthConfig } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session extends SIWESession {
+    address: string;
+    chainId: number;
+    accessToken: string;
+  }
+  interface User {
+    accessToken: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    wrappedJWT: string;
+  }
+}
 
 export const nextAuthSecret = process.env.NEXTAUTH_SECRET;
 if (!nextAuthSecret) {
-  throw new Error("NEXTAUTH_SECRET is not set");
+  throw new Error('NEXTAUTH_SECRET is not set');
 }
 
 export const authConfig = {
   secret: nextAuthSecret,
   providers: [],
   session: {
-    strategy: "jwt",
+    strategy: 'jwt',
   },
   callbacks: {
+    jwt({ trigger, token, user }) {
+      /**
+       * On sign in or sign up, we wrap the main JWT returned by the core
+       * service in another JWT that is managed by Authjs.
+       */
+      if (trigger === 'signIn' || trigger === 'signUp') {
+        return {
+          ...token,
+          wrappedJWT: user.accessToken,
+        };
+      }
+      return token;
+    },
     session({ session, token }) {
       if (!token.sub) {
         return session;
       }
 
-      const [, chainId, address] = token.sub.split(":");
-      if (chainId && address) {
+      const [, chainId, address] = token.sub.split(':');
+      const wrappedJWT = token.wrappedJWT;
+      if (chainId && address && wrappedJWT) {
+        /**
+         * The following are added to session so that we know the user through
+         * the address and chainId, and can make requests to the core service
+         * using the access token
+         */
         session.address = address;
-        session.chainId = parseInt(chainId, 10);
+        session.chainId = Number.parseInt(chainId, 10);
+        session.accessToken = wrappedJWT;
       }
 
       return session;
--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,68 +1,86 @@
-import NextAuth from "next-auth";
-import credentialsProvider from "next-auth/providers/credentials";
-import { getAddressFromMessage, getChainIdFromMessage, type SIWESession } from "@reown/appkit-siwe";
-import { createPublicClient, http } from "viem";
+import {
+  getAddressFromMessage,
+  getChainIdFromMessage,
+} from '@reown/appkit-siwe';
+import NextAuth from 'next-auth';
+import 'next-auth/jwt';
+import credentialsProvider from 'next-auth/providers/credentials';
 
-import { authConfig } from "./auth.config";
+import { authConfig } from './auth.config';
 
-declare module "next-auth" {
-  interface Session extends SIWESession {
-    address: string;
-    chainId: number;
-  }
-}
-
+/**
+ * TODO: Move all configs into a validated configs module to avoid duplication
+ *
+ * https://github.com/pattern-tech/pattern-ui/issues/3
+ */
 export const nextAuthSecret = process.env.NEXTAUTH_SECRET;
 if (!nextAuthSecret) {
-  throw new Error("NEXTAUTH_SECRET is not set");
+  throw new Error('NEXTAUTH_SECRET is not set');
 }
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!projectId) {
-  throw new Error("NEXT_PUBLIC_PROJECT_ID is not set");
+  throw new Error('NEXT_PUBLIC_PROJECT_ID is not set');
+}
+
+const siweVerificationApi = process.env.SIWE_VERIFICATION_API;
+if (!siweVerificationApi) {
+  throw new Error('SIWE_VERIFICATION_API is not set');
 }
 
 const providers = [
   credentialsProvider({
-    name: "Ethereum",
+    name: 'Ethereum',
     credentials: {
       message: {
-        label: "Message",
-        type: "text",
-        placeholder: "0x0",
+        label: 'Message',
+        type: 'text',
+        placeholder: '0x0',
       },
       signature: {
-        label: "Signature",
-        type: "text",
-        placeholder: "0x0",
+        label: 'Signature',
+        type: 'text',
+        placeholder: '0x0',
       },
     },
     async authorize(credentials) {
       try {
         if (!credentials?.message) {
-          throw new Error("SiweMessage is undefined");
+          throw new Error('SiweMessage is undefined');
         }
         const { message, signature } = credentials as Record<string, string>;
         const address = getAddressFromMessage(message);
         const chainId = getChainIdFromMessage(message);
 
-        const publicClient = createPublicClient({
-          transport: http(
-            `https://rpc.walletconnect.org/v1/?chainId=${chainId}&projectId=${projectId}`
-          ),
+        const response = await fetch(siweVerificationApi, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            message,
+            signature,
+          }),
         });
-        const isValid = await publicClient.verifyMessage({
-          message,
-          address: address as `0x${string}`,
-          signature: signature as `0x${string}`,
-        });
-        // end o view verifyMessage
 
-        if (isValid) {
-          return {
-            id: `${chainId}:${address}`,
-          };
+        if (response.ok) {
+          const {
+            data: { access_token: accessToken },
+          } = await response.json();
+
+          if (accessToken) {
+            return {
+              id: `${chainId}:${address}`,
+              accessToken,
+            };
+          }
         }
+
+        /**
+         * TODO: Handle errors accordingly, and show the user what went wrong
+         *
+         * https://github.com/pattern-tech/pattern-ui/issues/4
+         */
 
         return null;
       } catch (e) {
