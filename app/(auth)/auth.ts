23854c38fba42c440495be928c43d608e8d1cc20
Mohammad Kermani <mkermani144@gmail.com>
2025-03-15 12:17:55 +0000

    chore: update TODO links

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -11,7 +11,7 @@ import { authConfig } from './auth.config';
 /**
  * TODO: Move all configs into a validated configs module to avoid duplication
  *
- * https://github.com/pattern-tech/pattern-ui/issues/3
+ * https://github.com/pattern-tech/pattern-app/issues/3
  */
 export const nextAuthSecret = process.env.NEXTAUTH_SECRET;
 if (!nextAuthSecret) {
@@ -75,7 +75,7 @@ const providers = [
         /**
          * TODO: Handle errors accordingly, and show the user what went wrong
          *
-         * https://github.com/pattern-tech/pattern-ui/issues/4
+         * https://github.com/pattern-tech/pattern-app/issues/4
          */
 
         return null;
